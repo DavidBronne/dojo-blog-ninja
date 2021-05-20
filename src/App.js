0d235ee27c7ed99208e58543a60288325b92e77e
David Bronne <davidbronne@gmail.com>
2021-05-20 13:17:30 +0200

    Root component cleaned

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import './App.css';
 
 function App() {
   
-  const title = 'Welccome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com"
-
   return (
     <div className="App">
      <div className="content">
-       <h1>{title}</h1>
-       <p>Liked {likes} times</p>
-       <p>{10}</p>
-       <p>{'hello'}</p>
-       <p>{Math.random() * 10}</p>
-       <p>{[1,2,3]}</p>
-       <a href={link}> Google site</a>
+       <h1>App Component</h1>
      </div>
     </div>
   );
