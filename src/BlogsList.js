f700b980011044a17ddf36676d20cd24331151c5
David Bronne <davidbronne@gmail.com>
2021-05-20 19:12:23 +0200

    fetch with useEffect

--- src/BlogsList.js
+++ src/BlogsList.js
@@ -1,4 +1,4 @@
-const BlogsList = ({blogs,title,handleDelete}) => {
+const BlogsList = ({blogs,title}) => {
     
 /*     const blogs = props.blogs;
     const title = props.title;
@@ -8,14 +8,14 @@ const BlogsList = ({blogs,title,handleDelete}) => {
     return ( 
         <div className="blogs-list">
 
-                            <h2>{title}</h2>
+                            
             {
                 
                 blogs.map( (blog) => (
                        <div className="blog-preview" key={blog.id}>
                             <h2>{blog.title}</h2>
                             <p>{blog.author}</p>
-                            <button onClick={()=>handleDelete(blog.id)}>Delete</button>
+                            
                        </div>   
                 )
     
--- src/Home.js
+++ src/Home.js
@@ -3,37 +3,28 @@ import BlogsList from './BlogsList';
 
 const Home = () => {
     
-    const [blogs, setBlogs] = useState(
-        [
-            { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-          ]
-    )
+    const [blogs, setBlogs] = useState(null)
 
-    const [name, setName] = useState("Eliot");
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog)=>blog.id !== id);
-        setBlogs(newBlogs)
-    }
 
     useEffect(()=> {
-       console.log(blogs);
-       console.log('run useEffect');
        
-       
-    }, [name])
+       fetch(' http://localhost:8000/blogs')
+        .then(res =>{
+            return res.json()
+        })
+        .then(data => {
+            
+            setBlogs(data)
+            
+        })
+        
+        
+    }, [])
 
     return ( 
         <div className="home">
-
-            <BlogsList blogs={blogs} title='Global list' handleDelete={handleDelete}/>
-            {/* <BlogsList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's  movies"/> */}
-            <p>{ name }</p>
-            <button onClick={()=>setName('Coucou')}>Change name</button>
-
-        </div>
+            { blogs && <BlogsList blogs={blogs}  /> }
+         </div>
      );
 }
  
