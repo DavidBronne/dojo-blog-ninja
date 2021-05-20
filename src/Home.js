52d5875bade9808b0199aedeb3d3f0b3036dd26b
David Bronne <davidbronne@gmail.com>
2021-05-20 14:28:57 +0200

    onCLick + event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    
+    const handleCLick = (e) => {
+        console.log("Hello Eliot", e);   
+    }
+
+    const handleClickForMore = (name, e) => console.log('Hello ' + name , e.target);
+    
+
     return ( 
         <div className="home">
             <h1>Home</h1>
+            <button onClick={handleCLick}>Click me</button>
+            <button onClick={(e) => handleClickForMore('Coucou', e)}>Click for more</button>
         </div>
      );
 }
