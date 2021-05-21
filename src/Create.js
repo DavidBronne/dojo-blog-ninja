59904a0ea87dc749f1b403a363cd6dcfca26d0c8
David Bronne <davidbronne@gmail.com>
2021-05-21 18:10:25 +0200

    fetch post

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,68 @@
+import {useState} from 'react';
+
 const Create = () => {
+    
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Eliot');
+    const [isPending, setIsPending] = useState(false);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        const blog = {title , body, author}
+        console.log(blog);
+        setIsPending(true);
+
+        fetch ('http://localhost:8000/blogs', {
+                method: 'POST',
+                headers: {'Content-Type':'application/json'},
+                body: JSON.stringify(blog)
+            }
+        ).then(() => {
+            console.log('new blog added');
+            setIsPending(false)
+        })
+        
+    }
+    
     return ( 
         <div className="create">
             <h2>Create a blog</h2>
+            <form onSubmit={handleSubmit}>
+                
+                <label >Blog title: </label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange = {(e) => setTitle(e.target.value)}
+                />
+                
+                <label >Blog body: </label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                
+                <label >Blog author: </label>
+                <select
+                    value={author}
+                    onChange={ (e) => setAuthor(e.target.value)}
+                >    
+                    <option value="Eliot">Eliot</option>
+                    <option value="Cuicui">Cuicui</option>
+                </select>
+            
+            { !isPending && <button>Add</button>}
+            { isPending && <button disabled>is Loading...</button>}
+
+            </form>
+           
+            
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
         </div>
         
      );
