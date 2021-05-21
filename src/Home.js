3b09406a4748ce6208e67052a4db717bb34ba788
David Bronne <davidbronne@gmail.com>
2021-05-21 08:54:11 +0200

    Error handling

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,35 @@ const Home = () => {
     
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(()=> {
        
        fetch(' http://localhost:8000/blogs')
         .then(res =>{
+            if (!res.ok) {
+                throw Error ('Connected to server but response failed')
+            }
+            
             return res.json()
         })
         .then(data => {  
             setBlogs(data)
             setIsPending(false)
+            setError(null)
         })
-        
+        .catch(err => {
+            setError(err.message)
+            setIsPending(false)    
+        }
+        )
         
     }, [])
 
     return ( 
         <div className="home">
+            { error && <div> {error} </div>}
             { isPending && <div>Loading...</div>}
             { blogs && <BlogsList blogs={blogs}  /> }
          </div>
